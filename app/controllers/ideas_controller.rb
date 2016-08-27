def85afb60575a61936bd30e533957ca8a2200ee
Bryan Anderson <bryannobleanderson@live.com>
2016-08-27 12:30:25 -0600

    second push

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,13 +1,47 @@
 class IdeasController < ApplicationController
+  before_action :idea, except: [:index, :new, :create]
   def index
+    @ideas = Idea.all
   end
 
   def show
   end
 
   def new
+    @idea = Bucketlist.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.user_id = current_user.id
+    if @idea.save
+      redirect_to idea_path(@idea)
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    @idea.update(idea_params)
+    redirect_to idea_path(@idea)
+  end
+
+  def destroy
+    @idea.destroy
+    redirect_to ideas_path
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :description)
+  end
+
+  def idea
+    @idea = Idea.find(params[:id])
+  end
+
 end
